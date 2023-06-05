554334ffb63c14fb37de97a937c8296ae174d740
ikasoon <ikasoon119@gmail.com>
2023-06-05 15:30:55 +0900

    add mason lspconfig

--- init.lua
+++ init.lua
@@ -170,17 +170,22 @@ require("lazy").setup({
         },
     },
     -- vim-illuminate
+    -- Highlighting other uses of the word under the cursor
     "RRethy/vim-illuminate",
     -- mason
     {
         "williamboman/mason.nvim",
         build = ":MasonUpdate", -- :MasonUpdate updates registry contents
-        dependencies = { 
-            "neovim/nvim-lspconfig",
-            "williamboman/mason-lspconfig.nvim",
-            "jose-elias-alvarez/null-ls.nvim",
-        },
     },
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    -- "jose-elias-alvarez/null-ls.nvim",
+    -- nvim-cmp
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
 })
 
 -- Color Scheme
@@ -258,12 +263,94 @@ require('illuminate').configure({
     min_count_to_highlight = 1,
 })
 
-require("mason").setup()
+-- require("null-ls").setup({
+    -- sources = {
+        -- require("null-ls").builtins.formatting.stylua,
+        -- require("null-ls").builtins.diagnostics.eslint,
+        -- require("null-ls").builtins.completion.spell,
+    -- },
+-- })
 
-require("null-ls").setup({
-    sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
-        require("null-ls").builtins.completion.spell,
+require("mason").setup()
+local vim_map = function(type, key, value)
+	vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = false});
+end
+local custom_attach = function(client)
+    print("LSP started.");
+--     require'completion'.on_attach(client)
+--     require'diagnostic'.on_attach(client)
+
+    vim_map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+    vim_map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+    vim_map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+    vim_map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+    vim_map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+    vim_map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+    vim_map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+    vim_map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+    vim_map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+    vim_map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+    vim_map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+    vim_map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+    vim_map('n','<leader>r','<cmd>lua vim.lsp.buf.rename()<CR>')
+    vim_map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+    vim_map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+    vim_map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+end
+require("mason-lspconfig").setup_handlers {
+    function (server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {on_attach = custom_attach}
+    end
+}
+require("mason-lspconfig").setup()
+
+-- Set up nvim-cmp.
+local cmp = require'cmp'
+
+cmp.setup({
+    window = {
+        completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
     },
+    mapping = cmp.mapping.preset.insert({
+        -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- ['<C-c>'] = cmp.mapping.complete(),
+        -- ['<C-e>'] = cmp.mapping.abort(),
+        -- ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources(
+        {
+            { name = 'nvim_lsp' },
+        },
+        {{ name = 'buffer' },}
+    )
 })
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+        }, {
+        { name = 'cmdline' }
+    })
+})
+

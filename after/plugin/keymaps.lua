11ef247a329bbf508c018a30ce8d4b97b919023b
ikasoon <ikasoon119@gmail.com>
2023-07-05 01:29:43 +0900

    map arrow key to window move

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -1,8 +1,13 @@
 --------------------------------------------------------------------------------
 --                             My Custom Mapping                              --
 --------------------------------------------------------------------------------
-vim.keymap.set({'n', 'v'},           '<C-j>', '10j',           { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'},           '<C-k>', '10k',           { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'},           '<C-h>', ':bp<CR>',       { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'},           '<C-l>', ':bn<CR>',       { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<C-j>',   '10j',          { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<C-k>',   '10k',          { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<C-h>',   ':bp<CR>',      { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<C-l>',   ':bn<CR>',      { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<Up>',    '<C-w>k',       { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<Down>',  '<C-w>j',       { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<Left>',  '<C-w>h',       { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<Right>', '<C-w>l',       { noremap = true, silent = true })
+
 vim.keymap.set({'i', 'c', 'n', 'v', 'l', 'o', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })

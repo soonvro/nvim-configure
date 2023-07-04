6e7d063c8fecd46b79841a098512b7dc730c9c60
ikasoon <ikasoon119@gmail.com>
2023-07-05 00:04:31 +0900

    add telescope close key

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -5,4 +5,4 @@ vim.keymap.set({'n', 'v'},           '<C-j>', '10j',           { noremap = true,
 vim.keymap.set({'n', 'v'},           '<C-k>', '10k',           { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'},           '<C-h>', ':bp<CR>',       { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'},           '<C-l>', ':bn<CR>',       { noremap = true, silent = true })
-vim.keymap.set({'i', 'c', 'n', 'v'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })
+vim.keymap.set({'i', 'c', 'n', 'v', 'l', 'o', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })
--- init.lua
+++ init.lua
@@ -364,3 +364,13 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local actions = require("telescope.actions")
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-f>"] = actions.close
+      },
+    },
+  }
+}

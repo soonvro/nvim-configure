c7cc3eb2f933088e9c4690542a914c9b6e1ce78a
ikasoon <ikasoon119@gmail.com>
2023-07-05 22:46:42 +0900

    fix <esc> keymap on cmd mode

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -10,7 +10,10 @@ vim.keymap.set({'n', 'v'}, '<Down>',  '<C-w>j',       { noremap = true, silent =
 vim.keymap.set({'n', 'v'}, '<Left>',  '<C-w>h',       { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'}, '<Right>', '<C-w>l',       { noremap = true, silent = true })
 
-vim.keymap.set('', '<C-f>', '<Esc>',         { noremap = true, silent = true })
-vim.keymap.set('!', '<C-f>', '<Esc>',         { noremap = true, silent = true })
--- vim.keymap.set({'!', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })
--- vim.keymap.set({'i', 'c', 'n', 'v', 'l', 'o', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })
+-- esc key remap
+vim.keymap.set({'i', 'n', 'v', 'o', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })
+-- esc in command mode
+-- Note: In command mode mappings to esc run the command for some odd
+-- historical vi compatibility reason. We use the alternate method of
+-- existing which is Ctrl-C
+vim.keymap.set('c', '<C-f>', '<C-c>',         { noremap = true, silent = true })

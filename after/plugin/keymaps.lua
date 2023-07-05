01d1ef7185d18acd4fb9030dc88c8518e7d5dd6c
Soonhyung Kwon <sokwon@c2r4s3.42seoul.kr>
2023-07-05 20:47:04 +0900

    ongoing to fix esc keymap

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -10,4 +10,7 @@ vim.keymap.set({'n', 'v'}, '<Down>',  '<C-w>j',       { noremap = true, silent =
 vim.keymap.set({'n', 'v'}, '<Left>',  '<C-w>h',       { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'}, '<Right>', '<C-w>l',       { noremap = true, silent = true })
 
-vim.keymap.set({'i', 'c', 'n', 'v', 'l', 'o', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })
+vim.keymap.set('', '<C-f>', '<Esc>',         { noremap = true, silent = true })
+vim.keymap.set('!', '<C-f>', '<Esc>',         { noremap = true, silent = true })
+-- vim.keymap.set({'!', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })
+-- vim.keymap.set({'i', 'c', 'n', 'v', 'l', 'o', 't'}, '<C-f>', '<Esc>',         { noremap = true, silent = true })

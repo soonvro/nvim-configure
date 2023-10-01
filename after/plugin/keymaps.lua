28defe01055fdd2c16c2975a88b655a2d4b7c887
ikasoon <ikasoon119@gmail.com>
2023-10-01 15:38:51 +0900

    add keymap

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -13,10 +13,11 @@ vim.keymap.set({'n', 'v'}, '<C-j>',   '10j',          { noremap = true, silent =
 vim.keymap.set({'n', 'v'}, '<C-k>',   '10k',          { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'}, '<C-h>',   ':bp<CR>',      { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'}, '<C-l>',   ':bn<CR>',      { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'}, '<Up>',    '<C-w>k',       { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'}, '<Down>',  '<C-w>j',       { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'}, '<Left>',  '<C-w>h',       { noremap = true, silent = true })
-vim.keymap.set({'n', 'v'}, '<Right>', '<C-w>l',       { noremap = true, silent = true })
+
+vim.keymap.set({'n', 'v'}, '<Up>',    '<C-w>+',       { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<Down>',  '<C-w>-',       { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<Right>', '<C-w>>',       { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, '<Left>',  '<C-w><',       { noremap = true, silent = true })
 
 -- function keys
 vim.api.nvim_set_keymap('n', '<F8>', ':SymbolsOutline<CR>', { noremap = true, silent = false })

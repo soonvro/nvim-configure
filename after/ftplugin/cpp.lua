8c0afe33e31e228174aa927748773d3decbd116c
Soonhyung Kwon <sokwon@c2r5s1.42seoul.kr>
2023-10-25 21:15:41 +0900

    fix: make -j8

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -38,5 +38,5 @@ vim.api.nvim_create_user_command(
 --                             My Custom Mapping                              --
 --------------------------------------------------------------------------------
 -- function keys
-vim.api.nvim_set_keymap('n', '<F9>', ':!cd build && cmake .. && make && less<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F9>', ':!cd build && cmake .. && make -j8 && less<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<F10>', ':Execute<CR>',       { noremap = true, silent = true })

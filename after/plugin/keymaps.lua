fec0ba94718552d78cf14e51257cabee96a44e6d
Soonhyung Kwon <sokwon@c2r3s1.42seoul.kr>
2023-11-03 19:55:02 +0900

    feat: Move the screen so the cursor is centered after n,N

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -9,6 +9,9 @@ vim.keymap.set({'i', 'n', 'v', 'o', 't'}, '<C-f>', '<Esc>',         { noremap = 
 -- existing which is Ctrl-C
 vim.keymap.set('c', '<C-f>', '<C-c>',         { noremap = true, silent = true })
 
+vim.keymap.set({'n', 'v'}, 'n',   'nzz',          { noremap = true, silent = true })
+vim.keymap.set({'n', 'v'}, 'N',   'Nzz',          { noremap = true, silent = true })
+
 vim.keymap.set({'n', 'v'}, '<C-j>',   '3<C-e>',          { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'}, '<C-k>',   '3<C-y>',          { noremap = true, silent = true })
 vim.keymap.set({'n', 'v'}, '<C-h>',   ':bp<CR>',      { noremap = true, silent = true })

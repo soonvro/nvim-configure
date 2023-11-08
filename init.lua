2af34e204bec30017ebefeaee8ecfbd9fba7278a
Soonhyung Kwon <sokwon@c5r6s3.42seoul.kr>
2023-11-08 19:49:25 +0900

    not change

--- init.lua
+++ init.lua
@@ -151,10 +151,10 @@ require("lazy").setup({
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
-        dependencies = { 
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+          "MunifTanjim/nui.nvim",
         },
         keys = {{
             "<F6>",

134813d5621e4974a3809eaa353838adf9026e7b
ikasoon <ikasoon119@gmail.com>
2023-12-07 17:32:10 +0900

    feat: shift+F6 -> buffer neotree

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -6,17 +6,43 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  init = function() vim.g.neo_tree_remove_legacy_commands = 1 end,
-  keys = {{
-    "<F6>",
-    function()
-      require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-    end,
-    desc = "Explorer NeoTree (cwd)",
-  }},
-  opts = {
-    window = {
-      width = 30,
+  event = "VeryLazy",
+  -- init = function() vim.g.neo_tree_remove_legacy_commands = 1 end,
+  keys = {
+    {
+      "<F6>",
+      function()
+        vim.api.nvim_cmd({
+          cmd = "Neotree",
+          args = {
+            string.format("dir=%s", vim.loop.cwd()),
+            "toggle",
+            "reveal",
+          },
+        }, {})
+      end,
+      desc = "Explorer NeoTree (cwd)",
     },
+    {
+      "<F30>", -- ctrl+F6
+      function()
+        vim.api.nvim_cmd({
+          cmd = "Neotree",
+          args = {
+            "source=buffers",
+            "toggle",
+            "reveal",
+          },
+        }, {})
+      end,
+      desc = "Explorer NeoTree (buffers)",
+    }
   },
+  config = function()
+    require("neo-tree").setup{
+      window = {
+        width = 30,
+      },
+    }
+  end,
 }
